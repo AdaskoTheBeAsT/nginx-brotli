569c45c9cc4e4575b6a33ab265c312ac088109d4
Adam Pluciński <adaskothebeast@gmail.com>
2025-01-31 09:58:45 +0100

    non working

--- brotli/push.bat
+++ brotli/push.bat
@@ -1,4 +1,4 @@
-docker image tag adaskothebeast/nginx-brotli:latest adaskothebeast/nginx-brotli:v1.27.10-slim
+docker image tag adaskothebeast/nginx-brotli:latest adaskothebeast/nginx-brotli:v1.27.12-slim
 docker image tag adaskothebeast/nginx-brotli:latest adaskothebeast/nginx-brotli:v1.27-slim
 docker image tag adaskothebeast/nginx-brotli:latest adaskothebeast/nginx-brotli:v1-slim
 docker image push --all-tags adaskothebeast/nginx-brotli
