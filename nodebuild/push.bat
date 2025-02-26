7dc24ef6309849faabe152e6528310ec602d77b5
Adam Pluciński <adaskothebeast@gmail.com>
2025-02-27 00:52:21 +0100

    updates

--- nodebuild/push.bat
+++ nodebuild/push.bat
@@ -1,4 +1,4 @@
-docker image tag adaskothebeast/node-build:latest adaskothebeast/node-build:v1.2.0
+docker image tag adaskothebeast/node-build:latest adaskothebeast/node-build:v1.2.2
 docker image tag adaskothebeast/node-build:latest adaskothebeast/node-build:v1.2
 docker image tag adaskothebeast/node-build:latest adaskothebeast/node-build:v1
 docker image push --all-tags adaskothebeast/node-build
